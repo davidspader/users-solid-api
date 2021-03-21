04e2e67a973d9d7dcc12d31a61b88d08d313d961
David Spader Ambrozio <davidspader97@gmail.com>
2021-03-21 20:22:30 -0300

    all methods implemented

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -1,4 +1,4 @@
-import { Response, Request } from "express";
+import e, { Response, Request } from "express";
 
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
@@ -6,7 +6,15 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { name, email } = request.body;
+
+      const newUser = this.createUserUseCase.execute({ name, email });
+
+      return response.status(201).json(newUser);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,21 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("This user does not exist!");
+    }
+
+    if (user.admin) {
+      throw new Error("this user is already an admin!");
+    }
+
+    const updateUser = this.usersRepository.turnAdmin(user);
+
+    updateUser.updated_at = new Date();
+
+    return updateUser;
   }
 }
 
